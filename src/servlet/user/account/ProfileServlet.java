9e5a1b2c02fd20d0d72c2ec2821d9082c09a42c7
François Mathieu <maps_red@yahoo.fr>
2018-06-07 23:08:55 +0200

    Resolve #9 | Add profile page

--- src/servlet/user/account/ProfileServlet.java
+++ src/servlet/user/account/ProfileServlet.java
@@ -1,5 +1,7 @@
 package servlet.user.account;
 
+import utils.Manager;
+import utils.QueryBuilder;
 import utils.Routes;
 
 import javax.servlet.ServletException;
@@ -11,11 +13,17 @@ import java.io.IOException;
 
 @WebServlet(name = "ProfileServlet", urlPatterns = Routes.USER_ACCOUNT_PROFILE)
 public class ProfileServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (!Manager.isUserValid(request, response)) {
+            return;
+        }
 
+        System.out.println("doGet ProfileServlet");
+        int shortened_url = QueryBuilder.countComplexUrl((String) request.getSession().getAttribute("email"));
+        request.setAttribute("shortened_url", shortened_url);
+
+//        request.getSession().getAttribute("email")
+
+        this.getServletContext().getRequestDispatcher("/user/account/profile.jsp").forward(request, response);
     }
 }
